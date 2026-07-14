e0e174dcad742b767e8331694969922810119a6f
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-07-14 11:59:51 +0200

    feat: add navbar component

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import "../styles/Navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>Home</li>
+        <li>Collection</li>
+        <li>Cart</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
