d7aacaace7bced0d4984ea78975182e7ebeb5e20
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-07-14 12:01:38 +0200

    refactor: add navbar component to app layout

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,8 +1,14 @@
 import "./Header";
 import "../styles/App.css";
 import Header from "./Header";
+import Navbar from "./Navbar";
 
 function App() {
-  return <Header></Header>;
+  return (
+    <>
+      <Header></Header>
+      <Navbar></Navbar>
+    </>
+  );
 }
 export default App;
