0c258f9bf3704147d9001730803cd15f8b986283
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-07-15 17:21:38 +0200

    refactor: add hero component to app layout

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -2,12 +2,14 @@ import "./Header";
 import "../styles/App.css";
 import Header from "./Header";
 import Navbar from "./Navbar";
+import HeroCarousel from "./HeroCarousel";
 
 function App() {
   return (
     <>
       <Header></Header>
       <Navbar></Navbar>
+      <HeroCarousel></HeroCarousel>
     </>
   );
 }
