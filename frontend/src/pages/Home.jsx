c01def3df8290522a13da4b28944788a063f085b
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-07-22 11:56:32 +0200

    feat: add custom pages

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import HeroCarousel from "../components/HeroCarousel";
+import ShopByCategory from "../components/ShopByCategory";
+import WhyChooseVelora from "../components/WhyChooseVelora";
+
+export default function Home() {
+  return (
+    <>
+      <HeroCarousel />
+      <ShopByCategory />
+      <WhyChooseVelora />
+    </>
+  );
+}
