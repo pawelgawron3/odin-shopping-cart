59c56b3d29ad7a02d11d4e340b1b86de16759f3e
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-07-22 11:54:41 +0200

    refactore: use RouterProvider instead of App

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import routes from "./router/routes.jsx";
 import "./styles/reset.css";
-import App from "./components/App.jsx";
+
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
