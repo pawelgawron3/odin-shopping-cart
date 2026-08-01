6c81810e8f1bdf113150af23fd06cd8b814fcb3d
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-08-01 11:36:15 +0200

    chore: create custom hook

---
+++ frontend/src/hooks/useProducts.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import { getProducts } from "../services/productsApi";
+
+export default function useProducts() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const data = await getProducts();
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchProducts();
+  }, []);
+
+  return {
+    products,
+    loading,
+    error,
+  };
+}
