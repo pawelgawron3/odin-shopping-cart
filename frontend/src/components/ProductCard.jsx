9694f798978b772ce14fb949db7fddd11a99e969
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-08-01 11:17:32 +0200

    refactor: accept key as a prop, fix a bug

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,16 +1,16 @@
 import { Link } from "react-router";
 import styles from "../styles/ProductCard.module.css";
 
-export default function ProductCard({ product }) {
+export default function ProductCard({ key, product }) {
   return (
     <article className={styles.card}>
-      <Link to={`/products/${product.id}`} className={styles.imageWrapper}>
+      <Link to={`/products/${key}`} className={styles.imageWrapper}>
         <img src={product.image} alt={product.title} />
         <span className={styles.view}>View Details</span>
       </Link>
 
       <div className={styles.content}>
-        <h3>{product.title}</h3>
+        <h3>{product.name}</h3>
         <p>${product.price}</p>
       </div>
     </article>
