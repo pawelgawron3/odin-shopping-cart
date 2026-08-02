87f25219242eabd343ae9ad2158478ad1c631843
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-08-02 13:10:06 +0200

    feat: add ProductDetails content

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -1,7 +1,33 @@
 import { useParams } from "react-router";
+import useProduct from "../hooks/useProduct";
+import styles from "../styles/ProductDetails.module.css";
 
 export default function ProductDetails() {
   const { id } = useParams();
 
-  return <h1>Product {id}</h1>;
+  const { product, loading, error } = useProduct(id);
+
+  if (loading) {
+    return <p>Loading...</p>; // Add spinner
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  return (
+    <section className={styles.product}>
+      <img src={product.image} alt={product.name} />
+
+      <div className={styles.content}>
+        <h1>{product.name}</h1>
+
+        <p>{product.description}</p>
+
+        <span>${product.price}</span>
+
+        <button>Add to cart</button>
+      </div>
+    </section>
+  );
 }
