adcba2cf1ee54dd92788a4d6ebf8086c34ca6b86
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-08-02 12:43:20 +0200

    feat: fetch single product from API

--- frontend/src/services/productsApi.js
+++ frontend/src/services/productsApi.js
@@ -1,6 +1,7 @@
 import normalizeProduct from "../helpers/normalizeProduct";
 
 const API_URL = "https://dummyjson.com/products/category/sunglasses";
+const API_URL_PRODUCT_ID = "https://dummyjson.com/products/";
 
 export async function getProducts() {
   try {
@@ -14,5 +15,22 @@ export async function getProducts() {
     return data.products.map(normalizeProduct);
   } catch (err) {
     console.log(err);
+    throw err;
+  }
+}
+
+export async function getProductById(id) {
+  try {
+    const response = await fetch(API_URL_PRODUCT_ID + id);
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch a product");
+    }
+
+    const data = await response.json();
+    return normalizeProduct(data);
+  } catch (err) {
+    console.log(err);
+    throw err;
   }
 }
